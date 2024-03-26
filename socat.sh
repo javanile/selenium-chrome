f24d9a14b5821ef41be29da3068d10793749400e
francesco <info.francescobianco@gmail.com>
2024-03-26 19:19:24 +0100

    Introducing selenium

--- socat.sh
+++ socat.sh
@@ -3,9 +3,10 @@ set -e
 
 HOST_IP=$(traceroute "8.8.8.8" | grep -v "8.8.8.8" | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | head -1)
 
-echo "Starting socat over host IP: ${HOST_IP}"
-
 IFS=','
 for port in $LOCALHOST_PORTS; do
-    socat "TCP-LISTEN:${port},fork" "TCP:${HOST_IP}:${port}"
+  echo "Socat traffic to: ${HOST_IP}:${port}"
+  socat "TCP-LISTEN:${port},fork" "TCP:${HOST_IP}:${port}" &
 done
+
+while true; do sleep 1; done
